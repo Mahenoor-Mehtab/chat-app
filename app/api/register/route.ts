dc8ef552b6521e3c8fb5f37fd36b8a7d8fb1caea
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2026-04-07 17:55:04 +0530

    04:login-functionality

---
+++ app/api/register/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server"
+import bcrypt from "bcrypt"
+import  prisma  from "@/app/lib/prismadb"
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { name, email, password } = body
+
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { error: "Missing fields" },
+        { status: 400 }
+      )
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email }
+    })
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 409 }
+      )
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+       hashedPassword: hashedPassword
+      }
+    })
+
+    return NextResponse.json(
+      { message: "User created successfully", user },
+      { status: 201 }
+    )
+
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    )
+  }
+}
